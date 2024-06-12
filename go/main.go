e106e288adc05a0f0452111bbb1c3fa5a488575a
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2024-06-12 00:57:57 -0400

    start fetching post data

--- go/main.go
+++ go/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-	_ "syscall/js"
+	"log"
+	"os"
+	"path/filepath"
+	"syscall/js"
 )
 
-/*var (
+var (
 	document = js.Global().Get("document")
 	console  = js.Global().Get("console")
-	err error
-)*/
+	err      error
+)
 
 func getData(this js.Value, args []js.Value) interface{} {
-		//for posts in dir, build objecs
-		//return postData
+	type Post struct {
+		root *string
+		name *string
+		id   *uint
 	}
+
+	// Loop through the items that exist in the specified location. If its a directory, load data into objects, otherwise skip.
+	if err = filepath.Walk("../posts", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			var post Post
+
+			*post.root = path
+
+			log.Print("Found post directory: " + path)
+			console.Call("log", "Found post directory: "+path)
+			return nil
+		} else {
+			log.Println("Skipping " + path + " as it is not a post directory.")
+			console.Call("log", "Found post directory: "+path)
+			return nil
+		}
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
 
 func main() {
 	select {} // a `select` statement at the end of the `main()` function. This is necessary to prevent the Go program from exiting, as the WebAssembly binary will be terminated when the Go program exits.
