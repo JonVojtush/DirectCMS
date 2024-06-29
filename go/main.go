1f9957fd8ff731b7e53b99219364c403d2977416
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2024-06-28 22:26:42 -0400

    progress

--- go/main.go
+++ go/main.go
@@ -20,30 +20,38 @@ func getData(this js.Value, args []js.Value) interface{} {
 		id   *uint
 	}
 
+	type PostFile struct {
+		root *string
+		name *string
+		id   *uint
+	}
+
 	// Loop through the items that exist in the specified location. If its a directory, load data into objects, otherwise skip.
-	if err = filepath.Walk("../posts", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
+	if err = filepath.Walk("../posts", func(path string, posts os.FileInfo, err error) error {
+		if posts.IsDir() {
 			var post Post
 
 			*post.root = path
 
 			log.Print("Found post directory: " + path)
-			console.Call("log", "Found post directory: "+path)
+
+			if err = filepath.Walk(path, func(path string, postFile os.FileInfo, err error) error {
+					return nil
+				} else {
+					log.Println("Skipping " + path + " as it is not a post directory.")
+					return nil
+				}
+			}); err != nil {
+				log.Fatal(err)
+			}
+
 			return nil
 		} else {
 			log.Println("Skipping " + path + " as it is not a post directory.")
-			console.Call("log", "Found post directory: "+path)
 			return nil
-		}
-	}); err != nil {
+		} err != nil {
 		log.Fatal(err)
 	}
-
-	return nil
-}
 
 func main() {
 	select {} // a `select` statement at the end of the `main()` function. This is necessary to prevent the Go program from exiting, as the WebAssembly binary will be terminated when the Go program exits.
