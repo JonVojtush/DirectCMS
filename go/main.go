aa1f8a7dca89d1682f542f811ecf3d3f6b578114
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2024-07-04 18:05:50 -0400

    file servers

--- go/main.go
+++ go/main.go
@@ -1,9 +1,19 @@
+/*
+	! **Sitemap**: Generate a sitemap that lists all post URLs and media files. You can use tools like `go-sitemap-generator` to generate a sitemap dynamically.
+	! **Robots.txt**: Configure your server's `robots.txt` file to disallow crawling of media files but allow indexing of post pages.
+			User-agent: *
+			Disallow: /media/
+			Allow: /posts/
+			Sitemap: https://yourdomain.com/sitemap.xml
+*/
+
 package main
 
 import (
-	"log"
-	"os"
+	"io/ioutil"
+	"net/http"
 	"path/filepath"
+	"strings"
 	"syscall/js"
 )
 
@@ -13,46 +23,49 @@ var (
 	err      error
 )
 
-func getData(this js.Value, args []js.Value) interface{} {
-	type Post struct {
-		root *string
-		name *string
-		id   *uint
+func serveMedia(w http.ResponseWriter, r *http.Request) {
+	// Extract postId and imageName from the request URL
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 4 {
+		http.NotFound(w, r)
+		return
+	}
+	postId := parts[2]
+	imageName := parts[3]
+
+	// Construct the full path to the media file
+	mediaPath := filepath.Join("posts", postId, imageName)
+
+	// Serve the media file securely
+	http.ServeFile(w, r, mediaPath)
+}
+
+func servePostContent(w http.ResponseWriter, r *http.Request) {
+	// Extract postId from the request URL
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 3 {
+		http.NotFound(w, r)
+		return
+	}
+	postId := parts[2]
+
+	// Construct the full path to the content file
+	contentPath := filepath.Join("posts", postId, "content.md")
+
+	// Read and serve the markdown file
+	content, err := ioutil.ReadFile(contentPath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
 	}
 
-	type PostFile struct {
-		root *string
-		name *string
-		id   *uint
-	}
-
-	// Loop through the items that exist in the specified location. If its a directory, load data into objects, otherwise skip.
-	if err = filepath.Walk("../posts", func(path string, posts os.FileInfo, err error) error {
-		if posts.IsDir() {
-			var post Post
-
-			*post.root = path
-
-			log.Print("Found post directory: " + path)
-
-			if err = filepath.Walk(path, func(path string, postFile os.FileInfo, err error) error {
-					return nil
-				} else {
-					log.Println("Skipping " + path + " as it is not a post directory.")
-					return nil
-				}
-			}); err != nil {
-				log.Fatal(err)
-			}
-
-			return nil
-		} else {
-			log.Println("Skipping " + path + " as it is not a post directory.")
-			return nil
-		} err != nil {
-		log.Fatal(err)
-	}
+	w.Header().Set("Content-Type", "text/markdown; charset=utf-8")
+	w.Write(content)
+}
 
 func main() {
+	http.HandleFunc("/media/", serveMedia)
+	http.HandleFunc("/posts/", servePostContent)
+	http.ListenAndServe(":8080", nil)
 	select {} // a `select` statement at the end of the `main()` function. This is necessary to prevent the Go program from exiting, as the WebAssembly binary will be terminated when the Go program exits.
 }
--- web/scripts.js
+++ web/scripts.js
@@ -5,9 +5,29 @@ const go = new Go();
 
 //function setEventListeners() {}
 
+function renderPostContent(content) {
+  // Parse and render markdown content using a library like marked.js
+  const renderedContent = marked.parse(content);
+  document.getElementById('post-content').innerHTML = renderedContent;
+}
+
+async function fetchPostContent(postId) {
+  const response = await fetch(`/posts/${postId}/content.md`);
+  return await response.text();
+}
+
+async function fetchMedia(postId, imageName) {
+  const response = await fetch(`/media/${postId}/${imageName}`);
+  const blob = await response.blob();
+  const url = URL.createObjectURL(blob);
+  return url;
+}
+
 function init(wasmObj) {
   go.run(wasmObj.instance);
-  //getData()
+  //!fetchPostContent(postId)
+  //!fetchMedia(postId, imageName)
+  //! renderPostContent(content)
   //setEventListeners()
 }
 
